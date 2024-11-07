c058f962c96f81f744bf03b88a0b970cfcfe49da
theanalyticsarts <theanalyticsarts@gmail.com>
2024-11-07 10:39:05 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,15 @@
+import pandas as pd
+import numpy as np
+import sqlite3
+con = sqlite3.connect('db')
+items = pd.read_csv("https://raw.githubusercontent.com/theanalyticsarts/test/refs/heads/main/items.csv")
+sales = pd.read_csv("https://raw.githubusercontent.com/theanalyticsarts/test/refs/heads/main/sales.csv")
+
+items.to_csv('items.csv', index=False)
+sales.to_csv('sales.csv', index=False)
+
+items.to_sql('items', index=False, con=con)
+sales.to_sql('sales', index=False, con=con)
+
+print("Done.")
 
