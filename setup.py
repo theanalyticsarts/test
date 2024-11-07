fc0af4a134cf8380067ad5c6c5bbda8fab2c048d
theanalyticsarts <theanalyticsarts@gmail.com>
2024-11-07 11:01:48 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ con = sqlite3.connect('db')
 items = pd.read_csv("https://raw.githubusercontent.com/theanalyticsarts/test/refs/heads/main/items.csv")
 sales = pd.read_csv("https://raw.githubusercontent.com/theanalyticsarts/test/refs/heads/main/sales.csv")
 
-items.to_csv('items.csv', index=False)
+items.to_excel('items.xlsx', index=False)
 sales.to_csv('sales.csv', index=False)
 
 items.to_sql('items', index=False, con=con, if_exists='replace')
